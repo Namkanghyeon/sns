3a5277816fb2729abd4adbddc66fa104527858e6
2017029543 <2017029543@hanyang.ac.kr>
2022-01-29 16:24:52 +0900

    post feed

--- content/views.py
+++ content/views.py
@@ -15,20 +15,18 @@ class Main(APIView):
 
 class UploadFeed(APIView):
     def post(self, request):
-        # 일단 파일을 불러옴
         file = request.FILES['file']
         uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
-
+        # media 디렉토리에 이미지 파일 자체를 저장
         with open(save_path, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
-
         image = uuid_name
         content = request.data.get('content')
         user_id = request.data.get('user_id')
         profile_image = request.data.get('profile_image')
 
-        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image)
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
 
         return Response(status=200)
--- sns/settings.py
+++ sns/settings.py
@@ -125,4 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'  # 항상 /로 끝나도록 설정
 
 # 업로드된 파일을 저장할 디렉토리 경로
-MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
