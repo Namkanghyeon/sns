dc7600e6d4d706a89aab0a34a81074a3b094c851
2017029543 <2017029543@hanyang.ac.kr>
2022-02-03 15:18:24 +0900

    allowed host update

--- sns/settings.py
+++ sns/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-x&j#c6t*(j)b)i!aaf^r362cu4oqv3-ljdn$4_^f!mhu=&jy(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 # Application definition
 
